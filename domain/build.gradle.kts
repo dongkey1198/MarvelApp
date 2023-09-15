adfaafd38bdff2d8d45d277e571ae2325e051e3f
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-09-15 22:34:33 +0900

    chore: domain 모듈에 hilt 의존성 추가 (#5)

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,13 +1,21 @@
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
+    kotlin("kapt")
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
 
+    implementation("com.google.dagger:hilt-core:2.44")
+    kapt("com.google.dagger:hilt-compiler:2.44")
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
 }
