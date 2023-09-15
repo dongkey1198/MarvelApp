2f9b48c3514eae4908d90db782c360388db9faba
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-09-16 08:43:19 +0900

    feat: MainActivity에 viewBinding 적용 (#7)

--- presentation/src/main/java/com/example/marvelapp/view/MainActivity.kt
+++ presentation/src/main/java/com/example/marvelapp/view/MainActivity.kt
@@ -2,14 +2,17 @@ package com.example.marvelapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.marvelapp.R
+import com.example.marvelapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
