07b53875cd73e0c78eb17b5d76f053ade61d5c38
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-09-16 14:12:39 +0900

    feat: RecyclerView 초기화 코드 추가 (#9)

--- presentation/src/main/java/com/example/marvelapp/view/search/SearchFragment.kt
+++ presentation/src/main/java/com/example/marvelapp/view/search/SearchFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.marvelapp.databinding.FragmentSearchBinding
+import com.example.marvelapp.view.search.adapter.CharacterListAdapter
 
 class SearchFragment : Fragment() {
 
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
+    private val characterListAdapter by lazy { CharacterListAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +21,18 @@ class SearchFragment : Fragment() {
     ): View {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        binding.recyclerViewMarvelCharacter.apply {
+            adapter = characterListAdapter
+            layoutManager = GridLayoutManager(requireContext(), 2)
+        }
     }
 
     override fun onDestroyView() {
