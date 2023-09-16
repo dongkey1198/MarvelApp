b78de01f38a5242a286509fa58ab8a9991a569dc
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-09-16 09:03:50 +0900

    feat: FavoriteFragment 추가 (#7)

--- presentation/src/main/java/com/example/marvelapp/view/favorite/FavoriteFragment.kt
+++ presentation/src/main/java/com/example/marvelapp/view/favorite/FavoriteFragment.kt
@@ -1,22 +1,22 @@
-package com.example.marvelapp.view.search
+package com.example.marvelapp.view.favorite
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.marvelapp.databinding.FragmentSearchBinding
+import com.example.marvelapp.databinding.FragmentFavoriteBinding
 
-class SearchFragment : Fragment() {
+class FavoriteFragment : Fragment() {
 
-    private var _binding: FragmentSearchBinding? = null
+    private var _binding: FragmentFavoriteBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
         return binding.root
     }
 
