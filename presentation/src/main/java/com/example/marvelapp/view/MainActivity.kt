e03f52f23b2fec5bc34ada29384340a6bacb3ea0
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-09-16 11:02:34 +0900

    feat: ViewPager2와 TabLayout를 사용하여 탭기능 구현 (#7)

--- presentation/src/main/java/com/example/marvelapp/view/MainActivity.kt
+++ presentation/src/main/java/com/example/marvelapp/view/MainActivity.kt
@@ -3,16 +3,38 @@ package com.example.marvelapp.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.marvelapp.databinding.ActivityMainBinding
+import com.example.marvelapp.view.adapter.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewPagerAdapter by lazy { ViewPagerAdapter(supportFragmentManager, lifecycle) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViewPagerAdapter()
+        initTabLayout()
+    }
+
+    private fun initViewPagerAdapter() {
+        binding.viewPager.apply {
+            adapter = viewPagerAdapter
+        }
+    }
+
+    private fun initTabLayout() {
+        binding.tabLayout.apply {
+            TabLayoutMediator(this, binding.viewPager) { tab, position ->
+                tab.text = when (position) {
+                    0 -> "Search"
+                    else -> "Favorite"
+                }
+            }.attach()
+        }
     }
 }
