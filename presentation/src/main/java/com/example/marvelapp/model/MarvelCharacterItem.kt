d2dcf81db48c38cb4eda6a671d635f5904374c9e
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-09-17 19:37:22 +0900

    feat: MarvelCharacterItem 추가 (#13)

---
+++ presentation/src/main/java/com/example/marvelapp/model/MarvelCharacterItem.kt
@@ -0,0 +1,20 @@
+package com.example.marvelapp.model
+
+import com.example.domain.model.MarvelCharacter
+
+data class MarvelCharacterItem (
+    val id: Int,
+    val name: String,
+    val description: String,
+    val thumbnail: String,
+    val isFavorite: Boolean = false
+)
+
+fun MarvelCharacter.toPresentation() =
+    MarvelCharacterItem(
+        id = id,
+        name = name,
+        description = description,
+        thumbnail = thumbnail,
+        isFavorite = isFavorite
+    )
