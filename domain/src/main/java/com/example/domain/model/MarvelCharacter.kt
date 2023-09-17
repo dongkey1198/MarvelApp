907c157924702809f210e6bfc34bc5b6a1f2b4c6
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-09-17 18:21:29 +0900

    feat: MarvelCharacter 추가 (#13)

---
+++ domain/src/main/java/com/example/domain/model/MarvelCharacter.kt
@@ -0,0 +1,9 @@
+package com.example.domain.model
+
+data class MarvelCharacter(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val thumbnail: String,
+    val isFavorite: Boolean = false
+)
