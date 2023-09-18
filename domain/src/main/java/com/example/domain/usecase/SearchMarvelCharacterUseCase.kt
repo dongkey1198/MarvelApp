5b475f1fbd9b95280495d3c81766abb9ff844906
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-09-18 22:04:49 +0900

    feat: local db에서 캐릭터 목록 불러오는 기능 추가 (#20)

--- domain/src/main/java/com/example/domain/usecase/SearchMarvelCharacterUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/SearchMarvelCharacterUseCase.kt
@@ -1,52 +1,58 @@
 package com.example.domain.usecase
 
 import com.example.domain.model.MarvelCharacter
-import com.example.domain.model.RequestResult
 import com.example.domain.repository.MarvelRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 class SearchMarvelCharactersUseCase @Inject constructor(
     private val marvelRepository: MarvelRepository
 ) {
 
-    private val marvelCharacterList: MutableList<MarvelCharacter> = mutableListOf()
+    private val marvelCharactersCache: MutableList<MarvelCharacter> = mutableListOf()
     private var previousQuery: String = ""
-    private var total = 0
     private var offset = 0
+    private var total = 100
 
-    operator fun invoke(query: String): Flow<RequestResult<List<MarvelCharacter>>> = flow {
-        runCatching {
-            emit(RequestResult.Loading())
-            if (query.length < 2) {
-                Pair(emptyList(), 0)
-            } else if (previousQuery.checkIsNewSearch(query)) {
-                marvelRepository.fetchCharacters(query, 0)
-            } else if (marvelCharacterList.size >= total) {
-                Pair(emptyList(), total)
-            } else {
-                marvelRepository.fetchCharacters(query, offset + 10)
-            }
-        }.onSuccess { result ->
-            val marvelCharacters = result.first
-            if (previousQuery.checkIsNewSearch(query)) {
-                marvelCharacterList.clear()
+    suspend operator fun invoke(query: String): Flow<List<MarvelCharacter>> {
+        if (checkIsLastOffset(query)) return flowOf(marvelCharactersCache)
+        updateOffset(query)
+        return combine(
+            flowOf(marvelRepository.fetchCharacters(query, offset)),
+            marvelRepository.getMarvelCharacters()
+        ) { marvelDataSet, favorites ->
+            val characters = marvelDataSet.characters
+            total = marvelDataSet.total
+            if (query.checkIsNewSearch(previousQuery)) {
+                marvelCharactersCache.clear()
+                marvelCharactersCache.addAll(characters)
                 previousQuery = query
-                marvelCharacterList.addAll(marvelCharacters)
             } else {
-                marvelCharacterList.addAll(marvelCharacters)
+                marvelCharactersCache.addAll(characters)
             }
-            total = result.second
-            emit(RequestResult.Success(marvelCharacterList))
-            emit(RequestResult.Loading())
-        }.onFailure { exception ->
-            emit(RequestResult.Error("${exception.message}"))
-            emit(RequestResult.Loading())
+            marvelCharactersCache.map { it.copy(isFavorite = favorites.contains(it)) }
+        }
+    }
+
+    private fun updateOffset(query: String) {
+        offset = if (query.checkIsNewSearch(previousQuery)) {
+            0
+        } else {
+            offset + 10
+        }
+    }
+
+    private fun checkIsLastOffset(query: String): Boolean {
+        return when {
+            query.checkIsNewSearch(previousQuery) -> false
+            total <= marvelCharactersCache.size -> true
+            else -> false
         }
     }
 
     private fun String.checkIsNewSearch(query: String): Boolean {
-        return query != previousQuery
+        return this != query
     }
 }
