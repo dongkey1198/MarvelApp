99ad03d396b024a7b8d17250bf9b114d7e2827c3
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-09-19 13:33:40 +0900

    refactor: 로직 리팩토링 (#20)

--- domain/src/main/java/com/example/domain/usecase/SearchMarvelCharacterUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/SearchMarvelCharacterUseCase.kt
@@ -1,9 +1,11 @@
 package com.example.domain.usecase
 
 import com.example.domain.model.MarvelCharacter
+import com.example.domain.model.RequestResult
 import com.example.domain.repository.MarvelRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
@@ -11,48 +13,63 @@ class SearchMarvelCharactersUseCase @Inject constructor(
     private val marvelRepository: MarvelRepository
 ) {
 
-    private val marvelCharactersCache: MutableList<MarvelCharacter> = mutableListOf()
+    private var marvelCharactersCache: MutableList<MarvelCharacter> = mutableListOf()
     private var previousQuery: String = ""
     private var offset = 0
-    private var total = 100
 
-    suspend operator fun invoke(query: String): Flow<List<MarvelCharacter>> {
-        if (checkIsLastOffset(query)) return flowOf(marvelCharactersCache)
+    suspend operator fun invoke(query: String): Flow<RequestResult<List<MarvelCharacter>>> = flow {
         updateOffset(query)
-        return combine(
-            flowOf(marvelRepository.fetchCharacters(query, offset)),
-            marvelRepository.getMarvelCharacters()
-        ) { marvelDataSet, favorites ->
-            val characters = marvelDataSet.characters
-            total = marvelDataSet.total
-            if (query.checkIsNewSearch(previousQuery)) {
-                marvelCharactersCache.clear()
-                marvelCharactersCache.addAll(characters)
-                previousQuery = query
-            } else {
-                marvelCharactersCache.addAll(characters)
+        runCatching {
+            combine(
+                flowOf(marvelRepository.fetchCharacters(query, offset)),
+                marvelRepository.getMarvelCharacters()
+            ) { characters, favorites ->
+                updatePreviousQuery(query)
+                updateMarvelCharacterCache(query, characters, favorites)
+                marvelCharactersCache
+            }.collect {
+                emit(RequestResult.Success(it))
+                emit(RequestResult.Loading(false))
             }
-            marvelCharactersCache.map { it.copy(isFavorite = favorites.contains(it)) }
+        }.onFailure {
+            emit(RequestResult.Error( message = NETWORK_ERROR_MESSAGE))
+            emit(RequestResult.Loading(false))
         }
     }
 
     private fun updateOffset(query: String) {
-        offset = if (query.checkIsNewSearch(previousQuery)) {
-            0
-        } else {
-            offset + 10
+        offset = if (query.checkIsNewSearch(previousQuery)) 0 else offset + 10
+    }
+
+    private fun updatePreviousQuery(query: String) {
+        if (query.checkIsNewSearch(previousQuery)) {
+            marvelCharactersCache.clear()
+            previousQuery = query
         }
     }
 
-    private fun checkIsLastOffset(query: String): Boolean {
-        return when {
-            query.checkIsNewSearch(previousQuery) -> false
-            total <= marvelCharactersCache.size -> true
-            else -> false
+    private fun updateMarvelCharacterCache(
+        query: String,
+        newCharacter: List<MarvelCharacter>,
+        favoriteCharacters: List<MarvelCharacter>
+    ) {
+        if (query.checkIsNewSearch(previousQuery)) marvelCharactersCache.clear()
+        val result = (marvelCharactersCache + newCharacter.filterDuplicate()).map { character ->
+            character.copy(isFavorite = favoriteCharacters.any { fav -> character.id == fav.id })
         }
+        marvelCharactersCache.clear()
+        marvelCharactersCache.addAll(result)
     }
+
+    private fun List<MarvelCharacter>.filterDuplicate(): List<MarvelCharacter> = filterNot { newCharacters ->
+           marvelCharactersCache.any { currentCharacter -> currentCharacter.id == newCharacters.id }
+        }
 
     private fun String.checkIsNewSearch(query: String): Boolean {
         return this != query
+    }
+
+    companion object {
+        private const val NETWORK_ERROR_MESSAGE = "네트워크 에러로 검색 결과를 불러오지 못했습니다."
     }
 }
