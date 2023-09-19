6e11af281d42c60b519955aa4b8b5bc7257276dc
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-09-19 18:26:02 +0900

    feat: 캐릭터 삭제 기능 구현 (#29)

--- presentation/src/main/java/com/example/marvelapp/viewmodel/favorite/FavoriteViewModel.kt
+++ presentation/src/main/java/com/example/marvelapp/viewmodel/favorite/FavoriteViewModel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.domain.model.MarvelCharacter
 import com.example.domain.model.RequestResult
+import com.example.domain.usecase.DeleteMarvelCharacterUseCase
 import com.example.domain.usecase.GetFavoriteCharactersUseCase
+import com.example.marvelapp.extension.FlowExtensions.throttleFirst
 import com.example.marvelapp.model.MarvelCharacterItem
 import com.example.marvelapp.model.toPresentation
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,20 +15,26 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FavoriteViewModel @Inject constructor(
-    private val getFavoriteCharactersUseCase: GetFavoriteCharactersUseCase
+    private val getFavoriteCharactersUseCase: GetFavoriteCharactersUseCase,
+    private val deleteMarvelCharacterUseCase: DeleteMarvelCharacterUseCase
 ): ViewModel() {
+
+    private val _clickedCharacterFlow = MutableSharedFlow<MarvelCharacterItem>()
 
     private val _favoriteMarvelCharacterFlow = MutableStateFlow<List<MarvelCharacterItem>>(emptyList())
     val favoriteMarvelCharacterFlow get() = _favoriteMarvelCharacterFlow.asStateFlow()
 
-    private val _progressStateFlow = MutableStateFlow<Boolean>(false)
-    val progressStateFlow get() =  _progressStateFlow.asStateFlow()
+    private val _progressingStateFlow = MutableStateFlow<Boolean>(false)
+    val progressStateFlow get() =  _progressingStateFlow.asStateFlow()
 
     private val _resultMessageFlow = MutableSharedFlow<String>()
     val resultMessageFlow get() = _resultMessageFlow.asSharedFlow()
@@ -36,6 +44,13 @@ class FavoriteViewModel @Inject constructor(
 
     init {
         observeFavoriteCharacters()
+        observeClickedCharacter()
+    }
+
+    fun characterItemClicked(marvelCharacterItem: MarvelCharacterItem) {
+        viewModelScope.launch(Dispatchers.Default) {
+            _clickedCharacterFlow.emit(marvelCharacterItem)
+        }
     }
 
     private fun observeFavoriteCharacters() {
@@ -43,11 +58,25 @@ class FavoriteViewModel @Inject constructor(
             getFavoriteCharactersUseCase().collect { requestResult ->
                 when (requestResult) {
                     is RequestResult.Success -> updateFavoriteMarvelCharacterState(requestResult.data)
-                    is RequestResult.Loading -> updateProgressState(requestResult.isProgressing)
+                    is RequestResult.Loading -> updateProgressingState(requestResult.isProgressing)
                     is RequestResult.Error -> updateResultMassageState(requestResult.message)
                 }
             }
         }
+    }
+
+    private fun observeClickedCharacter() {
+        _clickedCharacterFlow
+            .throttleFirst()
+            .flatMapLatest { marvelCharacterItem -> deleteMarvelCharacterUseCase(marvelCharacterItem.id) }
+            .onEach { requestResult ->
+                when (requestResult) {
+                    is RequestResult.Success -> updateResultMassageState(requestResult.data)
+                    is RequestResult.Error ->  updateResultMassageState(requestResult.message)
+                    is RequestResult.Loading -> updateProgressingState(requestResult.isProgressing)
+                }
+            }
+            .launchIn(viewModelScope)
     }
 
     private fun updateFavoriteMarvelCharacterState(favoriteCharacters: List<MarvelCharacter>?) {
@@ -70,7 +99,7 @@ class FavoriteViewModel @Inject constructor(
         }
     }
 
-    private fun updateProgressState(isProgressing: Boolean) {
-        _progressStateFlow.update { isProgressing }
+    private fun updateProgressingState(isProgressing: Boolean) {
+        _progressingStateFlow.update { isProgressing }
     }
 }
