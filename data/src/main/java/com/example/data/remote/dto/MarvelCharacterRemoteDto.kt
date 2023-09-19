4e2cf02659b08dd5ea44a13f90a743162c379f21
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-09-19 13:39:47 +0900

    refactor: 미사용 Property 제거 (#20)

--- data/src/main/java/com/example/data/remote/dto/MarvelCharacterRemoteDto.kt
+++ data/src/main/java/com/example/data/remote/dto/MarvelCharacterRemoteDto.kt
@@ -4,16 +4,10 @@ import com.example.domain.model.MarvelCharacter
 import com.google.gson.annotations.SerializedName
 
 data class MarvelCharacterRemoteDto(
-    @SerializedName("code") val code: Int,
-    @SerializedName("status") val status: String,
     @SerializedName("data") val data: Data
 ) {
 
     data class Data(
-        @SerializedName("offset") val offset: Int,
-        @SerializedName("limit") val limit: Int,
-        @SerializedName("total") val total: Int,
-        @SerializedName("count") val count: Int,
         @SerializedName("results") val results: List<Result>
     )
 
