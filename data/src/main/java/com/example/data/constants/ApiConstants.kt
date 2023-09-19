f2486c59287e6246511c9dd7b5fbe42782338e22
Dong Hyeon Kim <donghyeon@happymoonday.com>
2023-09-19 19:35:53 +0900

    refactor: object 클래스로 변경 (#36)

--- data/src/main/java/com/example/data/constants/ApiConstants.kt
+++ data/src/main/java/com/example/data/constants/ApiConstants.kt
@@ -5,20 +5,19 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import java.sql.Timestamp
 
-class ApiConstants {
-    companion object {
-        private const val PRIVATE_KEY = BuildConfig.PRIVATE_KEY
-        const val API_KEY = BuildConfig.API_KEY
-        const val BASE_URL = "https://gateway.marvel.com"
-        const val limit = 10
-        val timeStamp = "${Timestamp(System.currentTimeMillis()).time}"
+object ApiConstants {
 
-        fun hash(): String {
-            val input = "$timeStamp$PRIVATE_KEY$API_KEY"
-            val md = MessageDigest.getInstance("MD5")
-            return BigInteger(1, md.digest(input.toByteArray()))
-                .toString(16)
-                .padStart(32, '0')
-        }
+    private const val PRIVATE_KEY = BuildConfig.PRIVATE_KEY
+    const val API_KEY = BuildConfig.API_KEY
+    const val BASE_URL = "https://gateway.marvel.com"
+    const val limit = 10
+    val timeStamp = "${Timestamp(System.currentTimeMillis()).time}"
+
+    fun hash(): String {
+        val input = "$timeStamp$PRIVATE_KEY$API_KEY"
+        val md = MessageDigest.getInstance("MD5")
+        return BigInteger(1, md.digest(input.toByteArray()))
+            .toString(16)
+            .padStart(32, '0')
     }
 }
